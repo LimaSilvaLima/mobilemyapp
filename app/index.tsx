cee29313efc2885c3aede26815d653b7c79e90c9
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2026-04-05 12:04:24 -0300

    ajustando configuraçoes iniciais do projeto

--- app/index.tsx
+++ app/index.tsx
@@ -1,80 +1,11 @@
-import { useEffect } from "react";
-import {
-  StyleSheet,
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
-} from "react-native";
-import * as SplashScreen from "expo-splash-screen";
-import { activateKeepAwakeAsync } from "expo-keep-awake";
-import { Stack } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
 
-import { Text, View } from "@/components/Themed";
-import { useColorScheme } from "@/components/useColorScheme";
-
-// Garante que a Splash Screen não suma antes da lógica inicial rodar
-SplashScreen.preventAutoHideAsync().catch(() => {});
-
-export default function WelcomeScreen() {
-  useEffect(() => {
-    if (__DEV__) {
-      // Solução para o aviso de depreciação
-      activateKeepAwakeAsync().catch(() => {});
-    }
-
-    SplashScreen.hideAsync().catch(() => {});
-  }, []);
-
+export default function Index() {
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={{ flex: 1 }}
-    >
-      <Stack.Screen options={{ headerShown: false }} />
-
-      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <View style={styles.container}>
-          <View style={styles.main}>
-            <Text style={styles.title}>Bem-vindo!</Text>
-            <Text style={styles.subtitle}>
-              Esta é a sua nova página inicial (index.tsx).
-            </Text>
-
-            <View
-              style={styles.separator}
-              lightColor="#eee"
-              darkColor="rgba(255,255,255,0.1)"
-            />
-          </View>
-        </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
+    <View>
+      <Text>Bem-vindo!</Text>
+      <Text>Esta é a sua nova página inicial (index.tsx).</Text>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
